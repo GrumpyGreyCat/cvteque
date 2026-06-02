e902220fba780cc27f8765f6cf4aa28e9584ebcb
Wendybest912 <filofilorial@gmail.com>
2026-06-02 18:10:57 +0200

    commit done

--- front/src/page/PartnerSearch.jsx
+++ front/src/page/PartnerSearch.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import './PartnerSearch.css';
+import './partnerSearch.css';
 import HeaderBanner from '../components/HeaderBanner';
 import PartnerCard from '../components/PartnerCard';
 import { mockPartners } from '../data/mockData';
